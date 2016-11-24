3627f56ba3591127d934557aac70b5e07d28be6b
Carsten Behring <carsten.behring@gmail.com>
2016-11-24 10:44:06 +0100

    converted to R package

--- R/extractTables.R
+++ R/extractTables.R
@@ -1,23 +1,19 @@
 
-library(xml2)
 
-library(tidyverse)
-library(openxlsx)
-
-
-
-##' Extracts all xml <tables...> into a list of data frames,
-##' where each dataframe is the table content. 
-##'
-##' @title 
-##' @param fileName The file to convert
-##' @return List of data frames
+#' Extracts all xml <tables...> nodes into a list of data frames,
+#' where each dataframe is the 'cell' nodes inside <table > content.
+#'
+#' @importFrom magrittr %>%
+#' @param fileName The file to convert
+#' @return List of data frames, where the key is a unique table name
+#' constructed from the xml attributes 'Name' and 'Caption'
+#' @export
 extractTables <- function(fileName) {
     ## read xml document
 
     document <- xml2::read_xml(fileName) %>%
-        xml_ns_strip
-    
+        xml2::xml_ns_strip()
+
     ## create all xpath expressions to search for
 
     xpaths <- c(".//Table",
@@ -28,78 +24,81 @@ extractTables <- function(fileName) {
     dfs <- list()
     ## iterate over all xpath expressions
     for (xpath in xpaths) {
-        tables <- xml_find_all(document,xpath)
+        tables <- xml2::xml_find_all(document,xpath)
         ## iterate over all elemnts found per expression
         for (table in tables) {
-                                       
+
             counter <- counter + 1
-            
+
             ## get table name and table caption
-            tableName <- xml_attr(table,"Name")
-            captionName <-  xml_attr(table,"Caption")
-            rowsTitle <- xml_attr(table,"RowsTitle")
+            tableName <- xml2::xml_attr(table,"Name")
+            captionName <-  xml2::xml_attr(table,"Caption")
+            rowsTitle <- xml2::xml_attr(table,"RowsTitle")
             if (tableName == "") {
                 tableName <- paste0("noName-",counter)
                 captionName <- "noCaption"
-                
+
             } else {
                 tableName <- paste0(counter,"-",tableName)
             }
             rowsTitle <- ifelse(rowsTitle=="","rows",rowsTitle)
             ## extract column names
             columnNames <- table %>%
-                      xml_find_all(".//Columns/Caption") %>%
-                      xml_text
+              xml2::xml_find_all(".//Columns/Caption") %>%
+              xml2::xml_text()
 
-                  ## extract group names
+                    ## extract group names
             groupNames <- table %>%
-                xml_find_all(".//Columns/Group") %>%
-                xml_text
-            
-                                        #extract row names 
+              xml2::xml_find_all(".//Columns/Group") %>%
+              xml2::xml_text()
+
+                                        #extract row names
             rowNames <- table %>%
-                xml_find_all(".//Rows/Caption") %>%
-                xml_text
-            
-            
-            
+              xml2::xml_find_all(".//Rows/Caption") %>%
+              xml2::xml_text()
+
+
+
             ## extract content of all cell as a vector
             cells <- table %>%
-                xml_find_all(".//Cells") %>%
-                xml_text
-            
+              xml2::xml_find_all(".//Cells") %>%
+              xml2::xml_text()
+
             ## convert cell content vector to matrix of row and columns
                                         #browser()
             cellMatrix <- matrix(cells,
                                 nrow = length(rowNames),
                                 ncol = length(columnNames),
                                 byrow = T)
-            
-                                   
+
+
 
             ## create data frame containing cells and (grouped) columnheaders
-            df <- data_frame(rowNumber=1:(length(rowNames)+2))
+            df <- dplyr::data_frame(rowNumber=1:(length(rowNames)+2))
             df[rowsTitle] <- c(rowsTitle,"",rowNames)
-            
-            
+
+
             df <- cbind(df,
-                       rbind(groupNames,columnNames,as_data_frame(cellMatrix))) 
+                       rbind(groupNames,columnNames,dplyr::as_data_frame(cellMatrix)))
             dfs[[tableName]] <- df %>%
                 dplyr::select(-rowNumber)
         }
-        
+
     }
     dfs
 }
-##' Writes the list of data frames to an excel file.
-##' All cells have type 'text', so needed to be converted in Excel, if needed.
-##' @param dfs A list of data frames
-exportToExcel <- function(dfs) {
-    openxlsx::write.xlsx(dfs,"tables.xlsx",colNames=F,rowNames=F)
-    print(paste(length(dfs),"sheet written"))    
+#' Writes the list of data frames to an excel file.
+#' All cells have type 'text', so needed to be converted in Excel, if needed.
+#' @param dfs A list of data frames
+#' @parama file The xlsx file to write the extracted tables to
+#' @return NULL
+#' @export
+exportToExcel <- function(dfs,file = "tables.xlxs") {
+    openxlsx::write.xlsx(dfs,"tables.xlsx",colNames = F,rowNames = F)
+    print(paste(length(dfs),"sheets written"))
 }
 
 
 ## The extratction can be run like this
 ## extractTables("./044312_Dinotefuran_Apple_Draft_Dul_RCK_June_7_2016.xml") %>%
-## exportToExcel()    
+## exportToExcel()
